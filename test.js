6c62b864bc00b3ecc8d4f2a009b9fe32530b95c1
Mike Zarra <mkzarra@Mikes-Air.fios-router.home>
2020-03-18 11:42:42 -0400

    added comments made minor fixes

--- test.js
+++ test.js
@@ -1,33 +1,36 @@
+// this function and doProblem2 have different scopes for "text".
+// you are also using "var" for both declartions which could cause issues related to scope.
+var text = function doProblem1() { // Why is this both a function declaration and variable declaration?
+  return "Hello, JavaScript!";
+};
 
-var text = function doProblem1() {return "Hello, JavaScript!"};
-	alert(text());
-
-
+alert(text());
 
 
 function doProblem2() {
 	var text = alert("Another message from JavaScript!");
-	
+	// There is no need to declare this variable in here unless you plan on using it again within the scope of this function, and you won't need to return anything because alert will execute when this function is called.
 }
 
 
 function doProblem3() {
 	
-	var makeitred = document.getElementById("target");
+  var makeitred = document.getElementById("target"); // unused variable.
+  // convention for JavaScript variables uses camelCase. makeitred should be makeItRed.
 	document.getElementById("target").innerHTML = "JavaScript is a dynamic language.";
-	/*makeitred.style.color = 'red';*/
-	document.getElementById('target').classList.add('makeitred');
-	
+	/*makeitred.style.color = 'red';*/ // unless you want to use makeitred here
+  document.getElementById('target').classList.add('makeitred'); // but it looks like makeitred is a class you add here.
+  // CSS convention used kebob-case. should be document.getElementById.('target').classList.add('make-it-red');
+  // You can remove any lines you aren't using.
 }	
 	
 
 function doProblem4() {
-	var hobbies = ["Technology", "Cars", "Sports"]
+  // I assume you know what's happening here, so this looks good. 
+	var hobbies = ["Technology", "Cars", "Sports"];
 	hobbies.push('Travel');
 	
 	for(var i = 0; i < hobbies.length; i++) {
-	alert(hobbies[i]);
+    alert(hobbies[i]);
 	}
-	
-	
 }
